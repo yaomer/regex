68776d2a9042064043a3446300dc9c3c755a6fa1
妖冶如你 <yaoyeruni@gmail.com>
2021-11-16 03:03:39 +0800

    Abstract the builder

--- regex.h
+++ regex.h
@@ -8,64 +8,6 @@
 
 namespace regex {
 
-// 添加显式的联结符'.'
-// ab -> a.b
-// REQ: 合法的正则表达式expr
-std::string add_concat_operator(const std::string& expr)
-{
-    std::string res;
-    int n = expr.size();
-    for (int i = 0; i < n; i++) {
-        char c = expr[i];
-        res.push_back(c);
-        if (c == '(' || c == '|') continue;
-        if (i + 1 < n) {
-            if (strchr("*?+|)", expr[i+1])) continue;
-            res.push_back('.');
-        }
-    }
-    return res;
-}
-
-// 将中缀表达式转为后缀表达式
-// REQ: 合法的中缀表达式
-std::string to_postfix(const std::string& expr)
-{
-    static std::unordered_map<char, int> op_prior = {
-        { '|' , 0 },
-        { '.' , 1 },
-        { '?' , 2 },
-        { '*' , 2 },
-        { '+' , 2 },
-    };
-    std::string res;
-    std::stack<char> stk;
-    for (auto c : expr) {
-        if (c == '|' || c == '.' || c == '?' || c == '*' || c == '+') {
-            while (!stk.empty() && stk.top() != '(' && op_prior[stk.top()] >= op_prior[c]) {
-                res.push_back(stk.top());
-                stk.pop();
-            }
-            stk.push(c);
-        } else if (c == '(') {
-            stk.push('(');
-        } else if (c == ')') {
-            while (!stk.empty() && stk.top() != '(') {
-                res.push_back(stk.top());
-                stk.pop();
-            }
-            stk.pop();
-        } else {
-            res.push_back(c);
-        }
-    }
-    while (!stk.empty()) {
-        res.push_back(stk.top());
-        stk.pop();
-    }
-    return res;
-}
-
 // 我们使用Thompson算法构造一个NFA，其构造出的NFA有以下特点：
 // 1) N(r)(由表达式r构成的NFA)有且只有一个开始状态和接受状态，开始状态没有入边，接受状态没有出边。
 // 2) N(r)中除接受状态以外的每个状态要么有一条symbol出边，要么至多有两条epsilon出边。
@@ -145,42 +87,117 @@ struct NFA {
     nfa_state *start, *end;
 };
 
-class matcher {
-public:
-    matcher(const std::string& expr) : nfa(build(expr)) {  }
-    bool match(const std::string& s)
+struct builder {
+    virtual NFA build(const std::string& expr) = 0;
+    virtual ~builder() = default;
+};
+
+struct postfix_builder : builder {
+    NFA build(const std::string& expr) override
     {
-        return search(nfa.start, s, 0);
-    }
-private:
-    NFA build(const std::string& expr)
-    {
-        auto postfix_expr = to_postfix(add_concat_operator(expr));
-        return build_from_postfix(postfix_expr);
-    }
-    NFA build_from_postfix(const std::string& postfix_expr)
-    {
+        NFA nfa;
         std::stack<NFA> stk;
-        for (auto c : postfix_expr) {
-            if (c == '*') {
+        auto postfix = to_postfix(add_concat_operator(expr));
+        for (auto c : postfix) {
+            switch (c) {
+            case '.':
+                nfa = stk.top(); stk.pop();
+                stk.top().concat(nfa);
+                break;
+            case '|':
+                nfa = stk.top(); stk.pop();
+                stk.top().Union(nfa);
+                break;
+            case '*':
                 stk.top().closure();
-            } else if (c == '|') {
-                auto right = stk.top(); stk.pop();
-                stk.top().Union(right);
-            } else if (c == '.') {
-                auto right = stk.top(); stk.pop();
-                stk.top().concat(right);
-            } else if (c == '?') {
+                break;
+            case '?':
                 stk.top().zero_or_one();
-            } else if (c == '+') {
+                break;
+            case '+':
                 stk.top().one_or_more();
-            } else {
+                break;
+            default:
                 stk.push(NFA(c));
+                break;
             }
         }
         assert(stk.size() == 1);
         return stk.top();
     }
+private:
+    // 添加显式的联结符'.'
+    // ab -> a.b
+    // REQ: 合法的正则表达式expr
+    std::string add_concat_operator(const std::string& expr)
+    {
+        std::string res;
+        int n = expr.size();
+        for (int i = 0; i < n; i++) {
+            char c = expr[i];
+            res.push_back(c);
+            if (c == '(' || c == '|') continue;
+            if (i + 1 < n) {
+                if (strchr("*?+|)", expr[i+1])) continue;
+                res.push_back('.');
+            }
+        }
+        return res;
+    }
+    // 将中缀表达式转为后缀表达式
+    // REQ: 合法的中缀表达式
+    std::string to_postfix(const std::string& expr)
+    {
+        static std::unordered_map<char, int> op_prior = {
+            { '|' , 0 },
+            { '.' , 1 },
+            { '?' , 2 },
+            { '*' , 2 },
+            { '+' , 2 },
+        };
+        std::string res;
+        std::stack<char> stk;
+        for (auto c : expr) {
+            if (c == '|' || c == '.' || c == '?' || c == '*' || c == '+') {
+                while (!stk.empty() && stk.top() != '(' && op_prior[stk.top()] >= op_prior[c]) {
+                    res.push_back(stk.top());
+                    stk.pop();
+                }
+                stk.push(c);
+            } else if (c == '(') {
+                stk.push('(');
+            } else if (c == ')') {
+                while (!stk.empty() && stk.top() != '(') {
+                    res.push_back(stk.top());
+                    stk.pop();
+                }
+                stk.pop();
+            } else {
+                res.push_back(c);
+            }
+        }
+        while (!stk.empty()) {
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        return res;
+    }
+};
+
+class matcher {
+public:
+    matcher() : builder(new postfix_builder()) {  }
+    matcher(const matcher&) = delete;
+    matcher& operator=(const matcher&) = delete;
+    void build(const std::string& expr)
+    {
+        nfa = builder->build(expr);
+    }
+    bool match(const std::string& s)
+    {
+        return search(nfa.start, s, 0);
+    }
+private:
     // 朴素的DFS，最坏时间复杂度为O(2^n)
     bool search(nfa_state *state, const std::string& s, int i)
     {
@@ -200,7 +217,7 @@ private:
         }
         return false;
     }
-
+    std::unique_ptr<builder> builder;
     NFA nfa;
 };
 
